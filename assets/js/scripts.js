e360d68fdc780cb99d06a2bd33cca4862991dbea
RegalStayLtd <mo@regalstay.co.uk>
2023-10-06 11:26:41 +0100

    holdings

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -73,6 +73,44 @@ $(function () {
     }
   });
 
+  $(".magnific-mix-gallery").each(function () {
+    var $container = $(this);
+    var $imageLinks = $container.find(".item");
+
+    var items = [];
+    $imageLinks.each(function () {
+      var $item = $(this);
+      var type = "image";
+      if ($item.hasClass("magnific-iframe")) {
+        type = "iframe";
+      }
+      var magItem = {
+        src: $item.attr("href"),
+        type: type,
+      };
+      magItem.title = $item.data("title");
+      items.push(magItem);
+    });
+
+    $imageLinks.magnificPopup({
+      mainClass: "mfp-fade",
+      items: items,
+      gallery: {
+        enabled: true,
+        tPrev: $(this).data("prev-text"),
+        tNext: $(this).data("next-text"),
+      },
+      type: "image",
+      callbacks: {
+        beforeOpen: function () {
+          var index = $imageLinks.index(this.st.el);
+          if (-1 !== index) {
+            this.goTo(index);
+          }
+        },
+      },
+    });
+  });
   /* ===============================  Swiper slider  =============================== */
 
   var parallaxSlider;
